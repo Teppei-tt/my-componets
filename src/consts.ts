cbbc214b48241fbc4b7629347c38d1a6c69687ab
Teppei <teppei.takata03@gmail.com>
2023-10-29 18:47:41 +0900

    ちょっと修正対応

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+// ページ全体に共有したいような、globalなデータを扱うときに、この構造は役に立つ
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'My Components';
+export const SITE_DESCRIPTION = 'コーディングの知見をどんどん貯めていくためにつくってます';
